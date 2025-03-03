042f2d5ada881826f0ce17384974f88045380560
Pupcheg <santos43241@gmail.com>
2025-03-03 17:11:31 +0300

    Removed static methods from ParameterSpecLookup

--- src/main/java/me/supcheg/adventurelike/processor/parameter/ParameterSpecLookup.java
+++ src/main/java/me/supcheg/adventurelike/processor/parameter/ParameterSpecLookup.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
@@ -24,12 +25,19 @@ public class ParameterSpecLookup {
         List<ParameterSpec> parameters = new ArrayList<>();
 
         for (ExecutableElement method : methodsIn(element.getEnclosedElements())) {
-            if (!method.isDefault() && method.getReturnType().getKind() != TypeKind.VOID) {
+            if (isValueMethod(method)) {
                 parameters.add(makeParameter(method));
             }
         }
 
         return parameters;
+    }
+
+    private boolean isValueMethod(@NotNull ExecutableElement method) {
+        return !method.isDefault()
+               && !method.getModifiers().contains(Modifier.STATIC)
+               && method.getReturnType().getKind() != TypeKind.VOID
+               && method.getParameters().isEmpty();
     }
 
     @NotNull
