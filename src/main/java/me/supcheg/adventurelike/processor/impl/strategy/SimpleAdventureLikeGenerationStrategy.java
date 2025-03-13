224d968e138b184f4bf32a8846323ba6fb1ebd0b
Pupcheg <santos43241@gmail.com>
2025-03-13 22:54:37 +0300

    Added Generated annotation generator to SimpleAdventureLikeGenerationStrategy

--- src/main/java/me/supcheg/adventurelike/processor/impl/strategy/SimpleAdventureLikeGenerationStrategy.java
+++ src/main/java/me/supcheg/adventurelike/processor/impl/strategy/SimpleAdventureLikeGenerationStrategy.java
@@ -4,7 +4,11 @@ import com.palantir.javapoet.TypeSpec;
 import lombok.RequiredArgsConstructor;
 import me.supcheg.adventurelike.processor.ElementProcessContext;
 import me.supcheg.adventurelike.processor.impl.step.ConstructorGenerationStep;
+import me.supcheg.adventurelike.processor.impl.step.GeneratedAnnotationGenerationStep;
+import me.supcheg.adventurelike.processor.value.ValueParameter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class SimpleAdventureLikeGenerationStrategy implements AdventureLikeGenerationStrategy {
@@ -12,11 +16,13 @@ public class SimpleAdventureLikeGenerationStrategy implements AdventureLikeGener
     @NotNull
     @Override
     public TypeSpec generate(@NotNull ElementProcessContext ctx) {
-        TypeSpec.Builder builder = TypeSpec.recordBuilder(ctx.element().getSimpleName() + "Impl")
+        TypeSpec.Builder target = TypeSpec.recordBuilder(ctx.element().getSimpleName() + "Impl")
                 .addSuperinterface(ctx.element().asType());
 
-        new ConstructorGenerationStep(ctx.valueParameters()).generate(builder);
+        List<ValueParameter> valueParameters = ctx.valueParameters();
+        new ConstructorGenerationStep(valueParameters).generate(target);
+        new GeneratedAnnotationGenerationStep(valueParameters).generate(target);
 
-        return builder.build();
+        return target.build();
     }
 }
